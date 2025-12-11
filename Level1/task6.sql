217c25917a66cb04832aa7e1d67f24e039091820
da1lyre <aidgowork@gmail.com>
2025-12-11 10:19:24 +0300

    Update task6.sql

--- Level1/task6.sql
+++ Level1/task6.sql
@@ -1,20 +1,20 @@
 -- Задание 6
 SELECT 
-    purchase.register_number,
-    gardener.last_name AS gardener_name,
-    nursery.name AS nursery_name
+    purchase.register_number AS "Номер ведомости",
+    gardener.last_name AS "Фамилия садовода",
+    nursery.name AS "Название питомника"
 FROM purchase
 JOIN gardener ON purchase.gardener_id = gardener.id
 JOIN nursery ON purchase.nursery_id = nursery.id
 ORDER BY purchase.register_number;
 
 SELECT 
-    purchase.register_number,
-    purchase.purchase_date,
-    gardener.last_name AS gardener_name,
-    gardener.discount,
-    plants.variety AS plant_name,
-    purchase.quantity
+    purchase.register_number AS "Номер ведомости",
+    purchase.purchase_date AS "Дата",
+    gardener.last_name AS "Фамилия садовода",
+    gardener.discount AS "Льгота",
+    plants.variety AS "Название растения",
+    purchase.quantity AS "Количество"
 FROM purchase
 JOIN gardener ON purchase.gardener_id = gardener.id
 JOIN plants ON purchase.plant_id = plants.id
