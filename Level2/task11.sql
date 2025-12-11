d6b38d484267fc0d9eb06af67af0c01cd4ee41f0
da1lyre <aidgowork@gmail.com>
2025-12-11 11:26:56 +0300

    Update task11.sql

--- Level2/task11.sql
+++ Level2/task11.sql
@@ -4,50 +4,45 @@ SELECT
     plants.price_rub AS "Стоимость"
 FROM plants
 WHERE id IN (
-    SELECT plant_id 
-    FROM purchase 
-    WHERE nursery_id IN (
+    SELECT plant_id FROM purchase WHERE nursery_id IN (
         SELECT id FROM nursery WHERE association = 'Дальнее'
     )
 )
 AND price_rub >= ALL (
-    SELECT price_rub 
-    FROM plants 
-    WHERE id IN (
-        SELECT plant_id 
-        FROM purchase 
-        WHERE nursery_id IN (
+    SELECT price_rub FROM plants WHERE id IN (
+        SELECT plant_id FROM purchase WHERE nursery_id IN (
             SELECT id FROM nursery WHERE association = 'Дальнее'
         )
     )
 );
 
 SELECT 
-    name AS "Название питомника",
-    association AS "Товарищество",
-    commission AS "Комиссионные"
-FROM nursery 
-WHERE nursery.association != 'Урожай'
-  AND NOT (nursery.commission <> ALL (
-      SELECT commission 
-      FROM nursery 
-      WHERE association = 'Урожай'
-  ))
-ORDER BY name;
-
-SELECT 
     last_name AS "Фамилия садовода",
     discount AS "Льгота"
 FROM gardener
-WHERE discount > (SELECT MIN(discount) FROM gardener);
+WHERE discount > ALL (
+    SELECT MIN(discount) FROM gardener
+);
+
+SELECT 
+    name AS "Название питомника",
+    association AS "Товарищество",
+    commission AS "Комиссионные"
+FROM nursery
+WHERE association != 'Урожай'
+  AND commission = ANY (
+      SELECT commission 
+      FROM nursery 
+      WHERE association = 'Урожай'
+  )
+ORDER BY name;
 
 SELECT 
     register_number AS "Номер ведомости",
     (SELECT last_name FROM gardener WHERE id = purchase.gardener_id) AS "Фамилия садовода",
-    purchase_date AS "Дата покупки",
-    total_cost AS "Сумма"
+    purchase_date AS "Дата покупки"
 FROM purchase
 WHERE total_cost >= ALL (
     SELECT 60000
 )
-    
+ORDER BY total_cost DESC;
