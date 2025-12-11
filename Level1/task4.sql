8e58c09f06870172eabedb57e70ddb2a65def9d7
da1lyre <aidgowork@gmail.com>
2025-12-11 10:16:04 +0300

    Update task4.sql

--- Level1/task4.sql
+++ Level1/task4.sql
@@ -1,15 +1,20 @@
 -- Задание 4
-SELECT DISTINCT association FROM gardener WHERE association IS NOT NULL
+SELECT DISTINCT association AS "Товарищество" 
+FROM gardener 
+WHERE association IS NOT NULL
 UNION
-SELECT DISTINCT association FROM nursery WHERE association IS NOT NULL
+SELECT DISTINCT association AS "Товарищество" 
+FROM nursery 
+WHERE association IS NOT NULL
 UNION
-SELECT DISTINCT where_from FROM plants WHERE where_from IS NOT NULL;
+SELECT DISTINCT where_from AS "Товарищество" 
+FROM plants 
+WHERE where_from IS NOT NULL;
 
-SELECT DISTINCT where_from AS association_source 
+SELECT DISTINCT where_from AS "Откуда" 
 FROM plants 
 WHERE where_from IS NOT NULL 
 ORDER BY where_from;
 
-SELECT DISTINCT TO_CHAR(purchase_date, 'Month') AS month
+SELECT DISTINCT TO_CHAR(purchase_date, 'Month') AS "Месяц"
 FROM purchase;
-
