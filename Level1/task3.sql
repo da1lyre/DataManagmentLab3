aa7c1c1c47e3467c0dce8a4c327f8da91bb8d89b
da1lyre <aidgowork@gmail.com>
2025-12-11 10:12:17 +0300

    Update task3.sql

--- Level1/task3.sql
+++ Level1/task3.sql
@@ -1,9 +1,32 @@
 -- Задание 3
-SELECT id, last_name, association, discount FROM gardener;
+SELECT 
+    id AS "Идентификатор", 
+    last_name AS "Фамилия", 
+    association AS "Товарищество", 
+    discount AS "Льгота,%"
+FROM gardener;
 
-SELECT id, name, association, commission FROM nursery;
+SELECT 
+    id AS "Идентификатор", 
+    name AS "Название", 
+    association AS "Товарищество", 
+    commission AS "Комиссионные,%"
+FROM nursery;
 
-SELECT id, variety, where_from, price_rub, seedling_count FROM plants;
+SELECT 
+    id AS "Идентификатор", 
+    variety AS "Сорт", 
+    where_from AS "Откуда", 
+    price_rub AS "Цена, руб", 
+    seedling_count AS "Кол-во саженцев"
+FROM plants;
 
-SELECT register_number, purchase_date, gardener_id, nursery_id, plant_id, quantity, total_cost FROM purchase;
-
+SELECT 
+    register_number AS "Номер ведомости", 
+    purchase_date AS "Дата", 
+    gardener_id AS "Садовод", 
+    nursery_id AS "Питомник", 
+    plant_id AS "Растение", 
+    quantity AS "Кол-во", 
+    total_cost AS "Итоговая стоимость, руб"
+FROM purchase;
